66230b29fd62780a0874097c0296dc55f0aaec6b
Kenan Ozbelli <kenan.ozbelli@gmail.com>
2020-02-13 12:21:48 -0500

    fixed bugs

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -5,12 +5,56 @@
 
 
 
-$("#search-form").sumbit(function(event){
+$("#search-form").on("submit", function(event){
     event.preventDefault();
+    console.log("submit");
+    
     var searchName = $("#search-term").val();
     var recordNumber = $("#record-number").val()
     var startYear = $("#start-year").val()
     var endYear=$("#end-year").val()
+    if(!startYear){
+        startYear = 19000101
+    }
+    if(!endYear){
+        endYear = 20201231
+    }
+    if (!recordNumber){
+        recordNumber = 12
+    }
+    if(!searchName){
+        alert("Please enter search term!")
+        return
+    }
+var queryUrl="https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+searchName+"&begin_date="+startYear+"&end_date="+endYear+"&api-key=EC5HXOCM9moGfxAAh4zTAuNx4TPwEu7M"
+
+$.get(queryUrl, function(news){
+    console.log(news.response)
+    for(var i = 0; i <news.response.docs.length;i++){
+
+    
+
+        var main = $("#mainContent");
+        var article = $("<div>")
+        var articleTitle = $("<div>")
+        articleTitle.text(news.response.docs[i].headline.main)
+        main.append(articleTitle)
+    }
+
 })
 
-var queryUrl="https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+searchName+"&begin_date="+startYear+"&end_date="+endYear+"&api-key=EC5HXOCM9moGfxAAh4zTAuNx4TPwEu7M"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
